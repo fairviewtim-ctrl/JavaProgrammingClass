185f38a5827eef1eabb168c83a6d68d5342d4603
Timothy Ross <fairviewtim@gmail.com>
2026-09-25 12:34:53 -0400

    Sept 25, 2026

--- src/Week1.java
+++ src/Week1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class test2 {
+public class Week1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("What is your name?");
---
+++ src/assignmentOne.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class assignmentOne { //public means that the class can be accessed from outside the class
+
+    public static void main(String[] args) { //main method, starts from here
+
+        //number# is the scanner output
+        //Scanner input is a string
+        Scanner num = new Scanner(System.in);
+        System.out.println("\tWhats the first number?"); //asks user for input
+        double number1 = num.nextDouble();  //converts string to double
+        System.out.println("\n \t What about the second number?");
+        double number2 = num.nextDouble();
+
+        double sum = number1 + number2;
+        System.out.println("\n\t=== Addition ===");
+        System.out.println(number1 + " + " + number2 + " + " + " = " + sum);
+
+        System.out.println("\n\t=== Subtraction ===");
+        System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));
+
+        System.out.println("\n\t=== Multiplication ===");
+        System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));
+
+        System.out.println("\n\t=== Division ===");
+        System.out.println(number1 + " / " + number2 + " = " + (number1 / number2));
+
+
+
+    }
+}
